2bdfb19947721cc09c8007f9608070c2299b98db
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-02-23 01:08:11 +0000

    Update video_driver.c

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -2661,7 +2661,7 @@ void video_driver_frame(const void *data, unsigned width,
       else 
          video_driver_crt_dynamic_super_width = false;
       
-      crt_switch_res_core(crt_swict_width, width, height, video_driver_core_hz, 
+      crt_switch_res_core(crt_switch_width, width, height, video_driver_core_hz, 
          video_info.crt_switch_resolution, video_info.crt_switch_center_adjust, 
             video_info.monitor_index, video_driver_crt_dynamic_super_width, 
                video_info.crt_debug_mode);
