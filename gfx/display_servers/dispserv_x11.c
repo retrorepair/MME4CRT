dc384b6b4bd51822e4654663e6b259440c8f1d6b
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-02-26 10:50:37 +0000

    Update dispserv_x11.c

--- gfx/display_servers/dispserv_x11.c
+++ gfx/display_servers/dispserv_x11.c
@@ -292,7 +292,7 @@ static bool x11_display_server_set_resolution(void *data,
       XRROutputInfo *outputs = XRRGetOutputInfo(dpy, res, res->outputs[monitor_index]);
       crt_mode = &crt_rrmode;
       //xid = res->outputs[monitor_index];
-      XID xid_mode = outputs->modes[crt_rrmode.id];
+      //XID xid_mode = outputs->modes[crt_rrmode.id];
       
       RROutput output = res->outputs[monitor_index];
 
